2d4b801e6a25ecd14958b5257bb7b96e61bb8cbd
xhp <601362529@qq.com>
2021-12-10 19:43:18 +0800

    xhp

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+/**
+ * @author xhp
+ * @PackageName PACKAGE_NAME
+ * @ClassName HelloWorld
+ * @Description
+ * @date 2021/12/10 7:39 PM
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
